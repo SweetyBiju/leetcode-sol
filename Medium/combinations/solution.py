4573a9f414588c31d8ed41cd3f165aa5f373af76
Sweety Biju <147310350+SweetyBiju@users.noreply.github.com>
2026-07-27 21:14:43 +0530

    Add solution: Combinations (Medium) - python3

---
+++ Medium/combinations/solution.py
@@ -0,0 +1,8 @@
+from itertools import combinations
+from typing import List
+
+
+class Solution:
+
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        return [list(c) for c in combinations(range(1, n + 1), k)]
